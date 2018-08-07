197615bc9e69722834627ed8c850c270c0a0cbcf
Riccardo Coppola <Riccardo.Coppola@thetrainline.com>
2018-08-08 01:35:09 +0200

    Adds `App` component

--- src/client/components/App/index.jsx
+++ src/client/components/App/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
-
+import Tabs from '../Tabs';
+import Tab from '../Tab';
 class App extends React.Component {
   render () {
-    return <p>Hello world</p>;
+    return (
+      <Tabs>
+        <Tab title="Bills">
+          <div>Bills content</div>
+        </Tab>
+        <Tab title="Potential bills">
+          <div>Potential bills content</div>
+        </Tab>
+      </Tabs>
+    );
   }
 }
 
